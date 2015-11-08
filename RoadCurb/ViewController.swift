f72198f1e70dbf6d17a7c4c96322fbf98dd3f15f
lincolnfaradey <iphoenix179@gmail.com>
2015-11-08 15:31:57 -0500

    fake data generator

--- RoadCurb/ViewController.swift
+++ RoadCurb/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 import MapKit
 import CoreLocation
 
+class Obstacle {
+    var date: NSDate
+    var userName: String
+    var coordintes: CLLocationCoordinate2D
+    
+    init(json: [String: AnyObject]) {
+        userName = json["user_name"] as! String
+        
+        let d = json["date"] as! Double
+        date = NSDate(timeIntervalSince1970:d/1000.0)
+        
+        let lat = json["latitude"] as! Double
+        let lon = json["longitude"] as! Double
+        coordintes = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+    }
+}
+
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
     @IBOutlet weak var obstacleBtn: UIButton!
@@ -21,6 +38,21 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         
         return clm
     }()
+
+    var obstacles = [Obstacle]()
+    var annotations = [MKPointAnnotation]()
+    
+    lazy var dateFormater: NSDateFormatter = {
+        let df = NSDateFormatter()
+        df.dateStyle = .ShortStyle
+        return df
+    }()
+    
+    lazy var mapClusterController: CCHMapClusterController = {
+        let mcc = CCHMapClusterController(mapView: self.mapView)
+        return mcc
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         clManager.delegate = self
@@ -31,20 +63,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         obstacleBtn.layer.masksToBounds = true
         
         obstacleBtn.userInteractionEnabled = false
-        
-        firebase.queryStartingAtValue(" ").observeEventType(.Value) { (snapshot: FDataSnapshot!) -> Void in
-            print("\(snapshot.key) -> \(snapshot.value)")
-//            for (_, v) in snapshot.value as! NSDictionary {
-//                if let dict = v as? NSDictionary {
-//                    let date = dict.valueForKey("date")
-//                    if let date = date as? NSNumber {
-//                        print("Date = \(NSDate(timeIntervalSince1970:date.doubleValue/1000.0))")
-//                    }
-//                }
-//            }
-        }
 
-        
         
     }
 
@@ -59,6 +78,25 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         
         mapView.setRegion(region, animated: true)
         obstacleBtn.userInteractionEnabled = true
+        print("coordinates \(userLocation.coordinate)")
+        firebase.queryOrderedByChild("latitude").queryStartingAtValue(region.center.latitude - 1).queryEndingAtValue(region.center.latitude + 1).observeEventType(.Value) { [unowned self] (snapshot: FDataSnapshot!) -> Void in
+            print("\(snapshot.key) -> \(snapshot.value)")
+            for (_, v) in snapshot.value as! NSDictionary {
+                let obstacle = Obstacle(json: v as! [String: AnyObject])
+                self.obstacles.append(obstacle)
+                guard (obstacle.coordintes.longitude > region.center.longitude - 1.0) &&
+                    (obstacle.coordintes.longitude < region.center.longitude + 1.0) else {
+                        continue
+                }
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = obstacle.coordintes
+                annotation.title = obstacle.userName
+                annotation.subtitle = self.dateFormater.stringFromDate(obstacle.date)
+                self.annotations.append(annotation)
+            }
+        
+            self.mapClusterController.addAnnotations(self.annotations, withCompletionHandler: nil)
+        }
     }
     
     @IBAction func obstacleBtnPressed(sender: UIButton) {
@@ -73,24 +111,28 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             "user_name": "User Name"
             ])
         
-//        let lon = 73984638
-//        let lat = 40759211
-//        
-//        
-//        srandom(UInt32(NSDate().timeIntervalSince1970))
-//        
-//        for i in 0...100 {
-//            let rand = random()
-//            let la = Double(rand * i % lat) / 1e6
-//            let lo = Double(rand * i % lon) / -1e6
-//            firebase.push([
-//                "latitude": la,
-//                "longitude": lo,
-//                "date": kFirebaseServerValueTimestamp,
-//                "user_name": "User Name"
-//                ])
-//        }
-     
+        
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            let lon = 100000
+            let lat = 100000
+            
+            for _ in 0...100 {
+                srandom(UInt32(NSDate().timeIntervalSinceNow))
+                
+                var rand = Int(arc4random())
+                let la = (Double(Int(coordinates.latitude * 10.0)) + (Double(rand % lat) / 1e5)) / 10.0
+                rand = Int(arc4random())
+                let lo = (Double(Int(coordinates.longitude * 10.0)) + (Double(rand % lon) / -1e5)) / 10.0
+                self.firebase.push([
+                    "latitude": la,
+                    "longitude": lo,
+                    "date": kFirebaseServerValueTimestamp,
+                    "user_name": "User Name"
+                    ])
+            }
+
+        })
+        
     }
 }
 
